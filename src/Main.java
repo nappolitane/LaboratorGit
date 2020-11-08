8c5c1e3aeef20335b8088127bb2160c492cfb9f7
cristiscraba <cristiscraba@gmail.com>
2020-11-08 21:03:43 +0200

    Implementare clasa Utilizator
    Signed-off-by: cristiscraba <cristiscraba@gmail.com>

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,6 @@ public class Main {
         //Exercitiu3.run();
         //Exercitiu4.run();
         //Exercitiu5.run();
-        Exercitiu6.run("D:\\Facultate\\Anul 4\\Ingineria programarii\\PracticLabs\\Laborator1\\ExercitiiLab1_Project\\src\\emailsFile.txt");
+        Exercitiu6.run("src/emailsFile.txt");
     }
 }
---
+++ src/Utilizator.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class Utilizator {
+    String nume;
+    ArrayList<Carte> cartiImprumutate;
+
+    public Utilizator(String nume) {
+        this.nume = nume;
+    }
+}
